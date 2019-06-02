2ac6ed527aa065f55ce45824b929b5a41b1fa004
Marcus Nielsen <me@marcusnielsen.org>
2019-06-02 10:27:27 +0200

    users can now login with the same password

--- server/src/components/auth/model.ts
+++ server/src/components/auth/model.ts
@@ -47,25 +47,60 @@ export async function createAuthModel(db: IDatabase<any>, saltRounds: number) {
       return result;
     }
 
-    const hash = await bcrypt.hash(validation.value.data.password, saltRounds);
-    const event = {
-      ...validation.value,
-      data: {
-        ...validation.value.data,
-        password: hash
+    const maybePassword = await db.oneOrNone(
+      `select data->'data'->>'password' as password from auth.events where data->'data'->>'nickname' = $<nickname>`,
+      {
+        nickname: validation.value.data.nickname
       }
-    };
+    );
 
-    await db.none(`insert into auth.events values ($<uuid>, $<data>)`, {
-      uuid: uuid(),
-      data: event
-    });
+    if (!maybePassword) {
+      const hash = await bcrypt.hash(
+        validation.value.data.password,
+        saltRounds
+      );
+      const event = {
+        ...validation.value,
+        data: {
+          ...validation.value.data,
+          password: hash
+        }
+      };
 
-    log.next({
-      type: "server#handle_message>succeeded",
-      cid: event.cid,
-      data: "User created successfully."
-    });
+      await db.none(`insert into auth.events values ($<uuid>, $<data>)`, {
+        uuid: uuid(),
+        data: event
+      });
+
+      log.next({
+        type: "auth#login>succeeded",
+        cid: props.cid,
+        data: "User logged in successfully."
+      });
+      return;
+    }
+
+    const password = maybePassword.password;
+
+    if (typeof password === "string") {
+      const passwordMatched = await bcrypt.compare(
+        validation.value.data.password,
+        password
+      );
+
+      if (!passwordMatched) {
+        log.next({
+          type: "auth#login>failed",
+          cid: props.cid,
+          data: "Could not login user."
+        });
+      }
+    } else {
+      console.log({ maybePassword });
+      throw new Error("Password was not a string.");
+    }
+
+    // TODO: Return JWT token (as http-only secure cookie?) or error to user.
   }
 
   return {
