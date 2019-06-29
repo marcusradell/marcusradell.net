a27b3eab64b1f089ab149fb35e4038d7194d910d
Marcus Nielsen <me@marcusnielsen.org>
2019-06-29 18:20:49 +0200

    adjust colors

--- web-app/src/modules/button/index.tsx
+++ web-app/src/modules/button/index.tsx
@@ -5,6 +5,9 @@ import { Color, Theme } from "../../theme";
 import { ButtonState } from "./types";
 export * from "./types";
 
+const colorScalar = 60;
+const backgroundColorScalar = 60;
+
 const style = (theme: Theme, color: Color) =>
   css({
     padding: "0 10",
@@ -12,21 +15,25 @@ const style = (theme: Theme, color: Color) =>
     borderRadius: "5px",
     font: "inherit",
     fontFamily: "Helvetica",
-    color: theme.colors.black(20),
-    backgroundColor: theme.colors[color](60),
+    color: theme.colors.black(colorScalar),
+    backgroundColor: theme.colors[color](backgroundColorScalar),
     cursor: "pointer",
     height: "50px",
     fontSize: "24px",
     width: "100%",
     transition: ".2s ease-in-out",
     transitionProperty: "color background-color",
+    boxShadow: "0 8px 6px -6px black",
     "&:active": {
-      color: theme.colors.black(20 - 10),
-      backgroundColor: theme.colors[color](60 - 10)
+      color: theme.colors.black(colorScalar + 10),
+      backgroundColor: theme.colors[color](backgroundColorScalar + 10),
+      boxShadow: "0 4px 3px -3px black"
     },
     "&:disabled": {
-      color: theme.colors.black(20 + 10),
-      backgroundColor: theme.colors[color](60 + 20)
+      cursor: "not-allowed",
+      color: theme.colors.black(colorScalar - 20),
+      backgroundColor: theme.colors[color](backgroundColorScalar - 20),
+      boxShadow: "0 0px 0px -0px black"
     }
   });
 
@@ -35,7 +42,10 @@ export function createButton(theme: Theme, color: Color, text: string) {
     const [state, setState] = useState<ButtonState>("enabled");
 
     return (
-      <button disabled={state === "disabled"} css={style(theme, color)}>
+      <button
+        disabled={state === "disabled" || color === "secondaryComplement"}
+        css={style(theme, color)}
+      >
         {text}
       </button>
     );
--- web-app/src/theme/colors/index.ts
+++ web-app/src/theme/colors/index.ts
@@ -1,6 +1,22 @@
-type CreateHslArgs = { h: number; s: number; l: number };
-const createHsl = ({ h, s = 70, l = 70 }: CreateHslArgs) =>
-  `hsl(${h}, ${easeIn(s / 100) * 100}%, ${easeIn(l / 100) * 100}%)`;
+const easeIn = (x: number) => (Math.cos(Math.PI * x + Math.PI) + 1) / 2;
+
+const calculateScaled = (from: number, to: number, scale: number) => {
+  const diff = to - from;
+  const easedScale = easeIn(scale / 100);
+  const result = from + diff * easedScale;
+
+  return result;
+};
+
+type Hsl = { h: number; s: number; l: number };
+
+type CreateHslArgs = { from: Hsl; to: Hsl; scale: number };
+const createHsl = ({ from, to, scale }: CreateHslArgs) =>
+  `hsl(${calculateScaled(from.h, to.h, scale)}, ${calculateScaled(
+    from.s,
+    to.s,
+    scale
+  )}%, ${calculateScaled(from.l, to.l, scale)}%)`;
 
 const rotate = (from: number, degrees: number) => (from + degrees) % 360;
 
@@ -9,22 +25,60 @@ const secondaryHue = rotate(primaryHue, 90);
 const complementHue = rotate(primaryHue, 180);
 const secondaryComplementHue = rotate(secondaryHue, 180);
 
-const black = (l: number) => createHsl({ h: primaryHue, s: 5, l });
+const fromHsl = (h: number) => ({
+  h,
+  s: 4,
+  l: 100
+});
 
-const primary = (l: number) => createHsl({ h: primaryHue, s: 50, l });
-const secondary = (l: number) => createHsl({ h: secondaryHue, s: 50, l });
-const complement = (l: number) => createHsl({ h: complementHue, s: 50, l });
-const secondaryComplement = (l: number) =>
-  createHsl({ h: secondaryComplementHue, s: 50, l });
+const toHsl = (h: number) => ({
+  h: h - 22,
+  s: 90,
+  l: 53
+});
 
-const easeIn = (x: number) => (Math.cos(Math.PI * x + Math.PI) + 1) / 2;
+const fromToHsl = (h: number) => ({
+  from: fromHsl(h),
+  to: toHsl(h)
+});
+
+const black = (scale: number) =>
+  createHsl({
+    from: { h: primaryHue, s: 5, l: 50 },
+    to: { h: primaryHue, s: 5, l: 10 },
+    scale
+  });
+
+const primary = (scale: number) =>
+  createHsl({
+    ...fromToHsl(primaryHue),
+    scale
+  });
+
+const secondary = (scale: number) =>
+  createHsl({
+    ...fromToHsl(secondaryHue),
+    scale
+  });
+
+const complement = (scale: number) =>
+  createHsl({
+    ...fromToHsl(complementHue),
+    scale
+  });
+
+const secondaryComplement = (scale: number) =>
+  createHsl({
+    ...fromToHsl(secondaryComplementHue),
+    scale
+  });
 
 export const colors = {
   black,
-  secondary: secondary,
-  complement: complement,
-  primary: primary,
-  secondaryComplement: secondaryComplement
+  secondary,
+  complement,
+  primary,
+  secondaryComplement
 };
 
 export type Color = keyof typeof colors;
